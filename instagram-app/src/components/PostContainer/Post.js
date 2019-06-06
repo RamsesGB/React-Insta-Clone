30211c1c3e1a0fce17be0d1c2393ed4bec5126cc
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-06-06 17:46:36 -0400

    Receiving data from PostContainer. Seperating it and sending it to its respective component destinations

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import '../PostContainer/PostContainer.css';
+import PostHeader from './PostHeader';
+import CommentSectionContainer from '../CommentSection/CommentSectionContainer';
 
 
 const Post = props => {
     return(
-        <div>
+        <div className='postBox'>
+            {/* Receiving data from PostContainer. Seperating it and sending it to its respective component destinations */}
+            <PostHeader
+            thumbnailImg={props.post.thumbnailUrl}
+            username={props.post.username}
+             />
             
+            <div className='postImgBox'>
+                <img
+                className='postImg'
+                src={props.post.imageUrl}
+                alt='IG post'
+                />
+            </div>
+
+            <CommentSectionContainer comments={props.post.comments}/>
         </div>
     );
 }
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -5,6 +5,7 @@ import Post from './Post';
 const PostContainer = props => {
     return(
         <div className='postContainer'>
+            {/* Iterating over the dummyData file and sending data down as props to the Post.js file */}
             {props.postsData.map(obj => <Post key={obj.imageUrl} post={obj} />)}
         </div>
     );
