222728bf4ceb5f6efea02a49f5d1fba275d024eb
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-06-06 18:43:09 -0400

    changed commentsectioncontainer to a class component. received data from post.js and sent it down to respective components

--- instagram-app/src/components/CommentSection/CommentSectionContainer.js
+++ instagram-app/src/components/CommentSection/CommentSectionContainer.js
@@ -2,14 +2,32 @@ import React from 'react';
 import '../CommentSection/CommentSection.css';
 import Comment from './Comment';
 import CommentInput from './CommentInput';
+// import PropTypes from 'prop-types';
 
-const CommentSectionContainer = props => {
-    return(
-        <div>
-            <Comment />
-            <CommentInput />
-        </div>
-    );
+class CommentSectionContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comments: props.comments
+        };
+    }
+    render() {
+        return (
+            <div>
+                {this.state.comments.map((obj, i) => <Comment key={i} comment={obj} />)}
+                <CommentInput />
+            </div>
+        )
+    }
+// Getting unexpected error. says the '.' in line 24 should be a ';'
+
+    // CommentSectionContainer.propTypes = {
+    //     comments: PropTypes.arrayOf(
+    //       PropTypes.shape({ text: PropTypes.string, username: PropTypes.string })
+    //     )
+    //   };
 }
 
+
+  
 export default CommentSectionContainer;
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../SearchBar/SearchBar.css';
-import IGLogo from '../../assets/iglogo.png';
+// import IGLogo from '../../assets/iglogo.png';
 
 const SearchBar = () => {
     return(
         <div className='searchBarContainer'>
             <div className='imgContainer'>
-                <img src={IGLogo} className='logoImg' alt='Insta logo'/>
+                {/* leaving the import of icons to another time */}
+                {/* <img src={IGLogo} className='logoImg' alt='Insta logo'/> */}
             </div>
             <div>
                 <input type='text' placeholder='Search' />
