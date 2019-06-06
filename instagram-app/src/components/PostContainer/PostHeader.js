e9f962d0bbc6d36f3f6e4466e5e4226e487daf60
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-06-06 17:53:15 -0400

    Creating the header for each IG post. receiving thumbnail and username from Post.js and displaying it

--- instagram-app/src/components/PostContainer/PostHeader.js
+++ instagram-app/src/components/PostContainer/PostHeader.js
@@ -3,8 +3,14 @@ import '../PostContainer/PostContainer.css';
 
 const PostHeader = props => {
     return(
+        // Creating the header for each IG post. receiving thumbnail and username from Post.js and displaying it
         <div>
-
+            <img
+                className='thumbnailImg'
+                src={props.thumbnailImg}
+                alt='User profile thumbnail'
+                />
+            <p>{props.username}</p>
         </div>
     );
 }
